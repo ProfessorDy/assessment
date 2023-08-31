153522a22d31a6f1ec373e54012249d0b73ac1a8
ProfessorDy <kingdeeyfx@gmail.com>
2023-08-31 17:25:50 +0100

    Fix issue again

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -7,7 +7,7 @@ const Summary = () => {
                 Join Our Newsletter: Stay Connected, Savor Exclusives!
             </h1>
 
-            <p className="mb-6 text-[1rem] font-normal leading-[1.375rem] tracking-[.0.03rem] opacity-80 ">Subscribe to our newsletter and be a part of the vibrant Rare Eat Fresh community. Stay connected with the latest updates, exclusive offers, and exciting promotions on rare West African food ingredients.</p>
+            <p className="mb-6 text-[1rem]">Subscribe to our newsletter and be a part of the vibrant Rare Eat Fresh community. Stay connected with the latest updates, exclusive offers, and exciting promotions on rare West African food ingredients.</p>
 
             <form className="flex flex-wrap gap-4 justify-center items-center">
                 <label htmlFor="">
