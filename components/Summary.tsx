7ce2746373f48d05bcb2c9160f83e5b8befa5cde
ProfessorDy <kingdeeyfx@gmail.com>
2023-08-31 17:23:02 +0100

    Fix deployment problem

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -6,7 +6,9 @@ const Summary = () => {
             <h1 className='text-[2rem] font-bold leading-normal tracking-[0.06rem] mb-4'>
                 Join Our Newsletter: Stay Connected, Savor Exclusives!
             </h1>
-            <p className="mb-6 text-[1rem] font-normal leading-[1.375rem] tracking-[.0.03rem] opacity-80 ">Subscribe to our newsletter and be a part of the vibrant Rare Eat Fresh community. Stay connected with the <br className="max-lg:hidden" />latest updates, exclusive offers, and exciting promotions on rare West African food ingredients.</p>
+
+            <p className="mb-6 text-[1rem] font-normal leading-[1.375rem] tracking-[.0.03rem] opacity-80 ">Subscribe to our newsletter and be a part of the vibrant Rare Eat Fresh community. Stay connected with the latest updates, exclusive offers, and exciting promotions on rare West African food ingredients.</p>
+
             <form className="flex flex-wrap gap-4 justify-center items-center">
                 <label htmlFor="">
                     <input className="py-[.625rem] px-5 rounded-[3.125rem] border border-[#131313] text-[1rem] font-normal leading-[1.375rem] tracking-[.03rem] " type="email" placeholder="Email address" />
