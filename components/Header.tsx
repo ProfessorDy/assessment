da5e20db5612381af226e8aa2fd8e170b21d87b2
ProfessorDy <kingdeeyfx@gmail.com>
2023-08-31 13:32:47 +0100

    Adjust mobile nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -38,30 +38,32 @@ const Header = () => {
             <Container>
 
                 <div className="flex justify-between items-center">
-                    <Sheet>
-                        <SheetTrigger>
-                            <AiOutlineMenu className="w-6 lg:hidden" />
-                        </SheetTrigger>
-                        <SheetContent side="left" className="w-[300px] sm:w-[400px]">
-                            <nav className="flex flex-col gap-4">
-                                {routes.map((route,) => (
-                                    <Link
-                                        key={route.label}
-                                        href={route.href}
-                                        className="block px-2 py-1 text-lg"
-                                    >
-                                        {route.label}
-                                    </Link>
-                                ))}
-                            </nav>
-                        </SheetContent>
-                    </Sheet>
-                    <Link href='/'>
-                        <Image
-                            src={Logo}
-                            alt="Logo"
-                        />
-                    </Link>
+                    <div className="flex items-center gap-4 sm:gap-8">
+                        <Sheet>
+                            <SheetTrigger>
+                                <AiOutlineMenu className="w-6 lg:hidden" />
+                            </SheetTrigger>
+                            <SheetContent side="left" className="w-[300px] sm:w-[400px]">
+                                <nav className="flex flex-col gap-4">
+                                    {routes.map((route,) => (
+                                        <Link
+                                            key={route.label}
+                                            href={route.href}
+                                            className="block px-2 py-1 text-lg"
+                                        >
+                                            {route.label}
+                                        </Link>
+                                    ))}
+                                </nav>
+                            </SheetContent>
+                        </Sheet>
+                        <Link href='/'>
+                            <Image
+                                src={Logo}
+                                alt="Logo"
+                            />
+                        </Link>
+                    </div>
                     <nav className="lg:flex gap-8 hidden ">
                         {routes.map((route) => (
                             <Link className="px-[.625rem] text-[#131313] hover:text-[#F36F00] transition-colors" key={route.label} href={route.href}>{route.label}</Link>
